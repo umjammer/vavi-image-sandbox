f8cf93193e51a741cebb5c32970e9c07076d4bb2
Naohide Sano <namachapanda2005@gmail.com>
2026-01-13 18:09:48 +0900

    [barcode] catch up w/ a dependency update

--- src/test/java/BarCode1d.java
+++ src/test/java/BarCode1d.java
@@ -9,21 +9,18 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.configuration.DefaultConfiguration;
-import org.apache.avalon.framework.logger.Logger;
 import org.krysalis.barcode4j.BarcodeClassResolver;
 import org.krysalis.barcode4j.BarcodeGenerator;
 import org.krysalis.barcode4j.BarcodeUtil;
 import org.krysalis.barcode4j.DefaultBarcodeClassResolver;
-import org.krysalis.barcode4j.cli.AdvancedConsoleLogger;
+import org.krysalis.barcode4j.configuration.DefaultConfiguration;
 import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 import org.krysalis.barcode4j.tools.MimeTypes;
-
 import vavi.awt.ImageComponent;
 
 
@@ -35,8 +32,7 @@ import vavi.awt.ImageComponent;
  */
 public class BarCode1d {
 
-    /** */
-    private static final Logger logger = new AdvancedConsoleLogger(AdvancedConsoleLogger.LEVEL_DEBUG, false, System.out, System.err);
+    private static final Logger logger = System.getLogger(BarCode1d.class.getName());
 
     /** */
     public static void main(String[] args) throws Exception {
@@ -54,26 +50,26 @@ public class BarCode1d {
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         String format = MimeTypes.expandFormat(MimeTypes.MIME_PNG);
-logger.debug("Generating " + format + "...");
+logger.log(Level.DEBUG, "Generating " + format + "...");
 
         BarcodeClassResolver resolver = new DefaultBarcodeClassResolver();
         BarcodeGenerator gen = BarcodeUtil.createBarcodeGenerator(getConfiguration(symbol), resolver);
 
-logger.debug("Resolution: " + dpi + "dpi");
+logger.log(Level.DEBUG, "Resolution: " + dpi + "dpi");
         BitmapCanvasProvider bitmap;
         if (bw) {
-logger.debug("Black/white image (1-bit)");
+logger.log(Level.DEBUG, "Black/white image (1-bit)");
             bitmap = new BitmapCanvasProvider(baos, format, dpi, BufferedImage.TYPE_BYTE_BINARY, false, 0); // TODO orientation = 0 ???
         } else {
-logger.debug("Grayscale image (8-bit) with anti-aliasing");
+logger.log(Level.DEBUG, "Grayscale image (8-bit) with anti-aliasing");
             bitmap = new BitmapCanvasProvider(baos, format, dpi, BufferedImage.TYPE_BYTE_GRAY, true, 0); // TODO orientation = 0 ???
         }
         gen.generateBarcode(bitmap, args[0]);
         bitmap.finish();
 
-logger.debug(args[0]);
+logger.log(Level.DEBUG, args[0]);
         Image image = ImageIO.read(new ByteArrayInputStream(baos.toByteArray()));
-logger.debug(image.toString());
+logger.log(Level.DEBUG, image.toString());
         JFrame frame = new JFrame();
 //      frame.setSize(320, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -81,7 +77,7 @@ logger.debug(image.toString());
         component.setImage(image);
         int w = image.getWidth(component);
         int h = image.getHeight(component);
-logger.debug("size: " + w + "x" + h);
+logger.log(Level.DEBUG, "size: " + w + "x" + h);
         component.setPreferredSize(new Dimension(w, h));
         frame.getContentPane().add(component);
         frame.pack();
@@ -91,7 +87,7 @@ logger.debug("size: " + w + "x" + h);
     /**
      *  "code128", "code39", "codabar", "ean-13", "ean-8", "intl2of5", "upc-a", "upc-e", "postnet"
      */
-    private Configuration getConfiguration(String symbol) {
+    private DefaultConfiguration getConfiguration(String symbol) {
         DefaultConfiguration config = new DefaultConfiguration("cfg");
         DefaultConfiguration child = new DefaultConfiguration(symbol);
         config.addChild(child);
